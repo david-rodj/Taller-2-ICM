a900573c3bd623dda3de5d2184b76b0171aaebf3
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-10-15 12:51:05 -0500

    Update MapStyleHelper.kt

--- app/src/main/java/com/example/taller2icm/utils/MapStyleHelper.kt
+++ app/src/main/java/com/example/taller2icm/utils/MapStyleHelper.kt
@@ -1,17 +1,38 @@
 package com.example.taller2icm.utils
 
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.MapTileIndex
 import org.osmdroid.views.MapView
 
 object MapStyleHelper {
+
+    // Tile source para modo claro (Mapnik - estándar de OpenStreetMap)
     fun applyLightStyle(mapView: MapView) {
         mapView.setTileSource(TileSourceFactory.MAPNIK)
     }
 
+    // Tile source para modo oscuro (CartoDB Dark Matter)
     fun applyDarkStyle(mapView: MapView) {
-        // Usar un tile source oscuro como CartoDB Dark Matter
-        // o configurar un overlay oscuro
-        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
-        // TODO: Implementar estilo oscuro personalizado
+        val darkTileSource = object : OnlineTileSourceBase(
+            "CartoDark",
+            0, 20, 256, ".png",
+            arrayOf(
+                "https://a.basemaps.cartocdn.com/dark_all/",
+                "https://b.basemaps.cartocdn.com/dark_all/",
+                "https://c.basemaps.cartocdn.com/dark_all/",
+                "https://d.basemaps.cartocdn.com/dark_all/"
+            )
+        ) {
+            override fun getTileURLString(pMapTileIndex: Long): String {
+                return baseUrl +
+                        MapTileIndex.getZoom(pMapTileIndex) + "/" +
+                        MapTileIndex.getX(pMapTileIndex) + "/" +
+                        MapTileIndex.getY(pMapTileIndex) +
+                        mImageFilenameEnding
+            }
+        }
+
+        mapView.setTileSource(darkTileSource)
     }
 }
